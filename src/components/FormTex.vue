436a42c8f49d71d770c9c2c464400c2d09224dc1
Oscar Galindo <oscargalindo@iso-152651.utep.edu>
2020-01-20 18:15:46 -0700

    Added an alert to let know the users to wait for submission confirmations.

--- src/components/FormTex.vue
+++ src/components/FormTex.vue
@@ -333,6 +333,7 @@ export default {
     sendImageToScript(){
       // Posts info to localhost on port 5000
       // Sanity check can be seen if navigated to that url
+      alert("Please wait for a confirmation, do not click the submit again until a message is received.")
       axios.post('http://129.108.156.19:8080/', {
         id: this.MinersID,
         imageID: this.copyID,
