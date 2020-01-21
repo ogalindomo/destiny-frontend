8780c78e8ab8232ed51d5020ef9e2527bfaeecad
Oscar Galindo <oscargalindo@Emma.local>
2020-01-21 11:33:13 -0700

    Adds a script that creates a metadata(.txt) file for the submission that is processed.

--- src/components/temp.py
+++ src/components/temp.py
@@ -1,24 +1,14 @@
-# import base64
-import sys
+import sys, json
 
-def process_image():
-    print(sys.argv[1])
+def load_vector(string_vector):
+    return json.loads(string_vector)
 
-
-
-
-#inputFile = open("/Users/oscargalindo/Desktop/Frontend/destiny-frontend/src/components/t.txt")
-#inputFile = inputFile.read()
-# print(inputFile)
-
-# img = open("/Users/oscargalindo/Desktop/Screen Shot 2019-11-01 at 6.19.13 PM.png", "rb")
-# img = base64.b64encode(img.read())
-# print(img)
-
-# fh = open("sample.png", "wb")
-# r = base64.b64decode(inputFile)
-# fh.write(r)
-# fh.close()
+def create_metadata(vector):
+    image_id = str(vector.pop('imageID'))
+    document = open("Gallery/"+image_id+'.txt', "w")
+    document.write(str(vector))
+    document.close()
 
 if __name__ == "__main__":
-    process_image()
+    vector = load_vector(sys.argv[1])
+    create_metadata(vector)
